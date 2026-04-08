a93a8a473acd82e4d2b903a9914df5471628f598
Ban <banking2359@gmail.com>
2026-04-08 18:47:10 +0300

    adding back arrow on all screen on the provider

--- src/app/(tabs)/_layout.js
+++ src/app/(tabs)/_layout.js
@@ -1,17 +1,20 @@
-import { Tabs } from "expo-router"
+import { router, Tabs } from "expo-router"
 import Ionicons from '@expo/vector-icons/Ionicons';
 export default function TabLayot() {
     return (
-        <Tabs>
+        <Tabs screenOptions={{
+            tabBarActiveTintColor: 'red',
+            headerLeft: () => <Ionicons name="chevron-back" size={30} color="black" style={{marginLeft:20,paddingHorizontal:5}} onPress={()=>router.push('/')} />
+        }}>
             <Tabs.Screen name="screenThree"
                 options={{
                     title: 'Home',
-                    tabBarIcon: () => <Ionicons name="home" size={24} color="black" />
+                    tabBarIcon: ({color}) => <Ionicons name="home" size={24} color={color} />
                 }} />
             <Tabs.Screen name="screenTwo"
                 options={{
-                    title:'setting',
-                    tabBarIcon: () => <Ionicons name="settings-outline" size={24} color="black" />
+                    title: 'setting',
+                    tabBarIcon: ({ color }) => <Ionicons name="settings-outline" size={24} color={color} />
                 }} />
         </Tabs>
     )
---
+++ src/component/topNavbar.js
@@ -0,0 +1,11 @@
+import { Ionicons } from "@expo/vector-icons";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { View } from "react-native";
+
+export default function navBar() {
+   return <SafeAreaView>
+        <View>
+            <Text style={{ color: 'black'}}>setting</Text>
+        </View>
+    </SafeAreaView>
+}
